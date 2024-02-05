5fa90c1f390873571c96eadc8d73a65316910980
Amr2812 <amr.osama.elmohamady@gmail.com>
2024-02-05 17:04:30 +0200

    Lab 3

--- lab3/timingtest/TimeAList.java
+++ lab3/timingtest/TimeAList.java
@@ -22,6 +22,23 @@ public class TimeAList {
     }
 
     public static void timeAListConstruction() {
-        // TODO: YOUR CODE HERE
+        AList<Integer> Ns = new AList<>();
+        AList<Double> times = new AList<>();
+
+        int[] ops = {1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000};
+        for (int op : ops) {
+            Stopwatch sw = new Stopwatch();
+            AList<Integer> test = new AList<>();
+
+            for (int j = 0; j < op; j++) {
+                test.addLast(1);
+            }
+
+            double timeInSeconds = sw.elapsedTime();
+            Ns.addLast(op);
+            times.addLast(timeInSeconds);
+        }
+
+        printTimingTable(Ns, times, Ns);
     }
 }
--- lab3/timingtest/TimeSLList.java
+++ lab3/timingtest/TimeSLList.java
@@ -22,7 +22,30 @@ public class TimeSLList {
     }
 
     public static void timeGetLast() {
-        // TODO: YOUR CODE HERE
+        AList<Integer> Ns = new AList<>();
+        AList<Integer> opsList = new AList<>();
+        AList<Double> times = new AList<>();
+
+        int ops = 10000;
+        int[] sizes = {1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000};
+        for (int size : sizes) {
+            SLList<Integer> test = new SLList<>();
+
+            for (int j = 0; j < size; j++) {
+                test.addLast(1);
+            }
+
+            Stopwatch sw = new Stopwatch();
+            for (int i = 0; i < ops; i++) {
+                test.addLast(1);
+            }
+            double timeInSeconds = sw.elapsedTime();
+            Ns.addLast(size);
+            times.addLast(timeInSeconds);
+            opsList.addLast(ops);
+        }
+
+        printTimingTable(Ns, times, opsList);
     }
 
 }
